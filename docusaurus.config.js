3c8a18a574837c376a97758440cef71d212c95bf
Nicolas DUBIEN <github@dubien.org>
2023-05-09 12:27:47 +0200

    Fix org deploy definition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,8 +19,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dubzzz', // Usually your GitHub org/user name.
+  projectName: 'draft-blog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
