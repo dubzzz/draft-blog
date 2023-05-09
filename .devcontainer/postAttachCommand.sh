c3139f36abd1f5b9ff0f10b6b944d777382742c7
Nicolas DUBIEN <github@dubien.org>
2023-05-09 12:32:36 +0000

    Move to create

--- .devcontainer/postAttachCommand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-# Start the dev server
-npm run start -- --host 0.0.0.0
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,6 @@ set -x
 
 # Install locked dependencies
 npm ci
+
+# Start the dev server
+npm run start -- --host 0.0.0.0
